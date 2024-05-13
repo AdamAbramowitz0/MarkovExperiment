7ec245635ef05d95cc4f429c351de2f3131208e0
Abhi Mummaneni <abhinav.mummaneni@tufts.edu>
2024-05-12 22:36:28 -0400

    trying random walk approach.

---
+++ RandomWalk.cpp
@@ -0,0 +1,37 @@
+#include "RandomWalk.h"
+#include <random>
+int main(int argc, char *argv[]) {
+    RandomWalk rw;
+    rw.train(argv[1]);
+    unsigned char input;
+    std::cin >> input;
+    rw.generate(input);
+}
+
+
+void RandomWalk::train(std::string filename) {
+    graph.resize(256);
+    for (int i = 0; i < graph.size(); ++i) {
+        graph[i].resize(256);
+        std::fill(graph[i].begin(), graph[i].end(), 0);
+    }
+    std::ifstream infile(filename);
+    unsigned char curr = infile.get();
+    for (unsigned char next = infile.get(); infile.good(); next = infile.get()) {
+        graph[curr][next]++;
+        curr = next;
+    }
+    
+}
+
+void RandomWalk::generate(unsigned char input) {
+    std::cout << std::endl;
+    std::random_device rd;
+    std::mt19937 gen(rd());
+    const int numWords = 100;
+    for (int i = 0; i < numWords; ++i) {
+        std::discrete_distribution<> d(graph[input].begin(), graph[input].end());
+        input = d(gen);
+        std::cout << input;
+    }
+}
---
+++ RandomWalk.h
@@ -0,0 +1,22 @@
+//
+// Created by Abhinav Mummaneni on 5/12/24.
+//
+
+#ifndef MARKOVEXPERIMENT_RANDOMWALK_H
+#define MARKOVEXPERIMENT_RANDOMWALK_H
+
+#include <vector>
+#include <fstream>
+#include <iostream>
+#include <string>
+class RandomWalk {
+public:
+    void train(std::string filename);
+    void generate(unsigned char input);
+private:
+    std::vector<std::vector<double> > graph;
+    
+};
+
+
+#endif //MARKOVEXPERIMENT_RANDOMWALK_H
